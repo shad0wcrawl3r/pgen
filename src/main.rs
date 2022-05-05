96d6358f54690601461f8f39f89ac46469d762c7
Ashwin <ashwin.belbase@cairav.net>
2022-05-05 12:51:26 +0545

    Added Symbols to generator

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ Usage:
 ")
 }
 
+const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789)(*&^%$#@!~";
+
 struct Config {
     length: u16
 }
@@ -28,17 +30,29 @@ impl Config{
     }
 }
 
-fn gen_alphanum(n: u16){
+fn gen_alphanum(n: u16) -> String{
     let randstring: String=thread_rng().sample_iter(&Alphanumeric).take(n.into()).map(char::from).collect();
-    println!("{}",randstring);
+    randstring
 }
+
+fn gen_rand_string(n:u16) -> String{
+    let mut rng = thread_rng();
+    let randstring: String = (0..n).map(|_|{ let idx = rng.gen_range(0..CHARSET.len());
+        CHARSET[idx] as char
+    }).collect();
+
+    randstring
+
+}
+
 fn main() {
     let arg: Vec<String> = args().collect();
     let conf = Config::new(&arg).unwrap_or_else(|err|{
         eprintln!("Some error occured. Error message: \n{}",err);
         process::exit(1)
     });
-    gen_alphanum(conf.length);
+    _ = gen_alphanum(conf.length);
+    println!("{}",gen_rand_string(conf.length));
 
     
 }
